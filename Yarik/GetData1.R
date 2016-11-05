77986bf47cbde8fced695157286a03d0926b1555
AlexTwoR <ash2r@yandex.ru>
2016-11-05 23:59:03 +0300

    Add Kalman

--- Yarik/GetData1.R
+++ Yarik/GetData1.R
@@ -2,6 +2,7 @@ data_raw=read.table(file = 'data/Data1.asc', header = TRUE)
 data<-data.frame(data_raw)
 head(data)
 
+
 data$Date<-as.POSIXct(paste(data$Дата, data$Время0), format="%d-%m %H:%M:%S")
 
 data$Время=NULL
@@ -22,5 +23,24 @@ mean(data_n$Value)
 
 
 
+#------- Пример ---------------
+library(dlm)
+library(quantmod)
 
+getSymbols("^GSPC", from="2010-09-01")
+y <- as.ts(Cl(GSPC))
+x <- dlmFilter(y, dlmModPoly(1, dV=10, dW=0.1))
+
+plot(y, type='l')
+lines(x$f[-1], col = "red")
+#Добавим EMA для сравнения
+lines(EMA(y), col = "green")
+
+#----------------
+
+y <- as.ts(data_n$Value)
+x <- dlmFilter(y, dlmModPoly(1, dV=10, dW=0.1))
+
+plot(y, type='l')
+lines(x$f[-1], col = "red")
 
